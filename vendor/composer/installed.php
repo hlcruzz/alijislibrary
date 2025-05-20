f142d177b5478ba7405fe1798f671cff04908f91
Christian Anthony Gemelo <xtian23anthony@gmail.com>
2025-05-20 16:59:26 +0800

    Updated verison

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'fbbc2ef7d9ba9698495291a82b35231e3abb97dd',
+        'reference' => '462a5376ac3357aaf8bab461c89a2cf13187feb9',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'fbbc2ef7d9ba9698495291a82b35231e3abb97dd',
+            'reference' => '462a5376ac3357aaf8bab461c89a2cf13187feb9',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
